1468075556163a201b764e40995ba1fe7886fe0d
nikhilninawe9 <nikhilninawe9@gmail.com>
2022-03-21 13:29:37 +0530

    commit

--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -2,6 +2,19 @@ package LinkedList;
 
 public class SinglyLinkedList {
 
+	private ListNode head;
+
+	private static class ListNode {
+		private int data;// generic but taken here as int
+		private ListNode next; // pointer to the next node
+
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+
+	}
+
 	public static void main(String[] args) {
 
 	}
