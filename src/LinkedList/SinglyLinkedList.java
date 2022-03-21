76d09eaab2800aedbcec74b7cd0b54531b4b485b
nikhilninawe9 <nikhilninawe9@gmail.com>
2022-03-21 13:49:19 +0530

    1

--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -12,11 +12,31 @@ public class SinglyLinkedList {
 			this.data = data;
 			this.next = null;
 		}
+	}
 
+	/* Printing the singly linked list */
+	public void display() {
+		ListNode current = head;
+		while (current != null) {
+			System.out.print(current.data + " -->");
+			current = current.next;
+		}
+		System.out.print("null");
 	}
 
 	public static void main(String[] args) {
+		/* 4 listnodes has been created */
+		SinglyLinkedList sl1 = new SinglyLinkedList();
+		sl1.head = new ListNode(10);
+		ListNode second = new ListNode(1);
+		ListNode third = new ListNode(8);
+		ListNode fourth = new ListNode(11);
 
+		/* connecting 4 listnodes forming chain and creating singly linked list. */
+		sl1.head.next = second;
+		second.next = third;
+		third.next = fourth;
+		sl1.display();
 	}
 
 }
